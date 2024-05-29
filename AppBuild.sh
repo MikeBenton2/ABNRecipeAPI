1b094be6df73ffcb80e9dc266e46709ae361f9a1
Michael Benton <michael.benton@nl.abnamro.com>
2024-05-29 16:31:46 +0200

    [develop] Setup Github actions

---
+++ AppBuild.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# shellcheck disable=SC2039
+package_project=${1:true}
+tag=${2:latest}
+
+if [ "$package_project" = true ]; then
+  echo 'Building ABN App with Maven'
+  ./mvnw clean package -DskipTests
+  echo '----------------------------------------------------------------------------------'
+  echo
+fi
+
+
+echo 'Building ABN App Docker Image'
+docker build -t ghcr.io/MikeBenton2/ABNRecipeAPI/abn-app:"$tag" -f ./infra/docker/DockerfileApp .
+echo '----------------------------------------------------------------------------------'
